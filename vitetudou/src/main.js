1ca226d8fa2a3356abeb9e60d90d154f892ed51a
wild_potato <78652566+sudoudou233@users.noreply.github.com>
2023-05-09 21:33:57 +0800

    啊
    坏了

---
+++ vitetudou/src/main.js
@@ -0,0 +1,33 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+
+import '@mdi/font/css/materialdesignicons.css'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const app = createApp(App)
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  theme: {
+    defaultTheme: 'dark'
+  },
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    }
+  },
+})
+
+app.use(vuetify)
+
+createApp(App).mount('#app')

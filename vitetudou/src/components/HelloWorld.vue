a5c5c7f0ec9ec0f7c235b6e2e28c8a37ed70a7be
wild_potato <78652566+sudoudou233@users.noreply.github.com>
2023-05-11 23:08:58 +0800

    未作出改变

--- vitetudou/src/components/HelloWorld.vue
+++ vitetudou/src/components/HelloWorld.vue
@@ -12,7 +12,7 @@ const count = ref(0)
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <button type="button" @click="count++">[这里is {{ count }}]</button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
@@ -24,7 +24,7 @@ const count = ref(0)
     <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
       >create-vue</a
     >, the official Vue + Vite starter
-  </p>
+  </p> 
   <p>
     Install
     <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
---
+++ vitetudou/src/components/Tudoumod.vue
@@ -0,0 +1,18 @@
+
+
+<script>
+    defineProps({
+        yijuhua:String,
+    })
+</script>
+
+<template>
+    <p>--------------------------</p>
+    <p>|</p>
+    <p>|</p>
+    <p>|    {{yijuhua}}</p>
+    <p>|</p>
+    <p>|</p>
+    <p>--------------------------</p>
+</template>
+
